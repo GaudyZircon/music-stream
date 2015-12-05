21afbaab7deb874703f4968ea1337b59120f0ad0
Gauthier Zirnhelt <GaudyZircon@users.noreply.github.com>
2015-12-05 23:58:21 +0100

    Refresh streams list when player is closed

--- music-stream.py
+++ music-stream.py
@@ -3,32 +3,34 @@ import subprocess
 
 LIMIT = 10
 PLAYER = 'vlc'
-url = 'http://streams.twitch.tv/kraken/streams?limit='+str(LIMIT)+'&offset=0&game=Music&broadcaster_language=&on_site=1'
+STREAMS_URL = 'http://streams.twitch.tv/kraken/streams?limit='+str(LIMIT)+'&offset=0&game=Music&broadcaster_language=&on_site=1'
 
-with urllib.request.urlopen(url) as response:
-    html = response.read().decode('utf8')
+while True:
+    with urllib.request.urlopen(STREAMS_URL) as response:
+        html = response.read().decode('utf8')
 
-i = 0
-urls = []
-for line in html.split(','):
-    if 'status' in line:
-        status = line.split('"')[-2]
-        status = ''.join(i for i in status if ord(i)<128) #filter non ascii characters
-    if 'display_name' in line:
-        name = line.split('"')[-2]
-        print(str(i) + ') ' + name + ' : ' + status)
-        i += 1
-    if 'url' in line:
-        url = line.split('"')[-2]
-        urls.append(url)
+    i = 0
+    urls = []
+    for line in html.split(','):
+        if 'status' in line:
+            status = line.split('"')[-2]
+            status = ''.join(i for i in status if ord(i)<128) #filter non ascii characters
+        if 'display_name' in line:
+            name = line.split('"')[-2]
+            print(str(i) + ') ' + name + ' : ' + status)
+            i += 1
+        if 'url' in line:
+            url = line.split('"')[-2]
+            urls.append(url)
 
-choice = LIMIT
-while (choice >= LIMIT):
-    choice = int(input('Choose a stream\n'))
+    choice = LIMIT
+    while (choice >= LIMIT):
+        choice = int(input('Choose a stream\n'))
 
-cmd = ['livestreamer', urls[choice], 'audio']
-if PLAYER != 'vlc':
-    cmd.append('-p')
-    cmd.append(PLAYER)
+    cmd = ['livestreamer', urls[choice], 'audio']
+    if PLAYER != 'vlc':
+        cmd.append('-p')
+        cmd.append(PLAYER)
 
-subprocess.Popen(cmd, shell=False)
+    subprocess.call(cmd, shell=False)
+    print('\n\n\n')
