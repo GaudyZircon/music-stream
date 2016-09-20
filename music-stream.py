68997493ebd63b33fbe02d191974a5d05575f02a
Gauthier Zirnhelt <GaussianTerror@users.noreply.github.com>
2016-09-20 22:35:57 +0200

    Also provide a client ID to livestreamer

--- music-stream.py
+++ music-stream.py
@@ -29,7 +29,7 @@ while True:
     while (choice >= LIMIT):
         choice = int(input('Choose a stream\n'))
 
-    cmd = ['livestreamer', urls[choice], 'audio']
+    cmd = ['livestreamer', '--http-header', 'Client-ID='+str(CLIENT_ID), urls[choice], 'audio']
     if PLAYER != 'vlc':
         cmd.append('-p')
         cmd.append(PLAYER)
